c4ce1a619292b6a79c073488ad1aefe07c3e1341
@Jerin <133591611+jerinakter99@users.noreply.github.com>
2024-06-12 20:42:08 +0600

    try
    try to

---
+++ try.py
@@ -0,0 +1,20 @@
+#*** Magic/Dunder/Special method
+# magic method work with object
+#based on action magic method called implicitly/atomatically
+# __str__ method  provide meaningful string representations of objects.
+
+class me:
+    def __init__(self,name,age,bio):
+        self.name = name
+        self.age = age
+        self.bio = bio
+    def __str__(self):                         #define the __str__ method in a class,
+     return f"Me {self.name},{self.age} years old ,{self.bio}"
+
+Me=me("Jerin Akter",24,"I am a Trader")
+print(Me)
+#or
+print(str(Me))
+
+
+
