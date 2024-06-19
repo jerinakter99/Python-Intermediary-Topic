109970d5044001c42b8994296c537b3b330a73b5
@Jerin <jerin.akter3033@gmail.com>
2024-06-19 18:37:13 +0600

    tuple,string

---
+++ 6.Immtable.py
@@ -0,0 +1,14 @@
+#cannot be changed after they are created.
+#Immutable Objects are of in-built datatypes like int, float, bool, string, Unicode, and tuple.
+
+tuple = (0, 1, 2, 3)
+tuple[0] = 4 #it will give error
+print(tuple)
+
+my_string = "Hello"
+print(my_string)
+# my_string[0] = 'h' it will give error
+
+
+
+
