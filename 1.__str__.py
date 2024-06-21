32000cc1dc2621d9d1388e5dd0360944be6b6ee2
@Jerin <jerin.akter3033@gmail.com>
2024-06-21 13:47:22 +0600

    __str__ method

--- 1.__str__.py
+++ 1.__str__.py
@@ -10,7 +10,7 @@ class me:
     def __str__(self):                         #define the __str__ method in a class,
      return f"Me {self.name},{self.age} years old ,{self.bio}"
 
-Me=me("Jerin Akter",24,"I am a Trader")
+Me=me("Jeny",23,"I am a Trader")
 print(Me)
 #or
 #print(str(Me))
