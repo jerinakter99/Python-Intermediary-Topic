ee229f9d9b5b9079313d1f0bf81009597f05c2ec
@Jerin <133591611+jerinakter99@users.noreply.github.com>
2024-07-18 11:54:57 +0600

    INT, VARCHAR, DATETIME, DECIMAL ,BOOLEAN

---
+++ Data Types.sql
@@ -0,0 +1,33 @@
+Data types are used to define the nature of data that can be stored in each column of a table.
+
+1.Numeric Data Types:
+  **Integer Types Storing integer values.
+Useful: Suitable for storing numerical data such as counts, IDs, and numerical attributes. It's a basic, yet essential, data type for many database applications.
+Example: INT
+ **Floating-Point 
+
+2.Date and Time Data Types:
+Storing date and time information.
+Useful for tracking events and timestamps in applications.It's precise and stores
+both date and time information in a format that is easily readable and sortable.
+DATE: Date in 'YYYY-MM-DD' format.
+DATETIME: Date and time in 'YYYY-MM-DD HH:MM' format.
+TIMESTAMP: Date and time, with automatic initialization and updating to the current date and time.
+TIME: Time in 'HH:MM' format.
+YEAR: Year in 'YYYY' format 
+
+3.String Data Types:
+CHAR: Fixed-length string. Size up to 255 characters.
+VARCHAR:  Storing variable-length strings Variable-length string. Size up to 65,535 characters.
+          Offers flexibility by allowing strings of varying lengths up to a specified maximum.
+          It's commonly used for text fields such as names, addresses, and descriptions.
+BINARY: Fixed-length binary data. Similar to CHAR.
+
+*** Data Types
+1. VARCHAR
+2. INT
+3. DATETIME
+4. DECIMAL
+5. BOOLEAN
+6. BLOB            Storing binary large objects such as images, videos, and other multimedia
+7. JSON            Storing JSON formatted data
